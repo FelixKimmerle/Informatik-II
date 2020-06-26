251bcc6a7c5488b131cde1a052efd322058c278b
Felix Kimmerle <FelixKimmerle@hotmail.com>
2020-06-26 13:59:23 +0200

    blatt7

---
+++ Blatt7/7_3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+void tausch(int *x, int *y)
+{
+    std::cout << "In tausch() vor dem Tausch: x = " << *x << " y = " << *y << std::endl;
+    int tmp = *x;
+    *x = *y;
+    *y = tmp;
+    std::cout << "In tausch() nach dem Tausch: x = " << *x << " y = " << *y << std::endl;
+}
+
+int main(int argc, char **argv)
+{
+    int x = 3;
+    int y = 6;
+    std::cout << "In main() vor dem Tausch: x = " << x << " y = " << y << std::endl;
+    tausch(&x, &y);
+    std::cout << "In main() nach dem Tausch: x = " << x << " y = " << y << std::endl;
+    return 0;
+}
